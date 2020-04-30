5d88d0cb088918fb908d8c1d5bb8c1500f2b9886
nicohnavarro <nicohnavarro@gmail.com>
2020-04-30 19:35:07 -0300

    003: Inicio: agregar rutas

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/angularapp'));
+
 app.listen(process.env.PORT || 8080);
 
-aoo.get('/*', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './components/pelicula/pelicula-alta/pelicula-alta.component';
+import { PeliculaListadoComponent } from './components/pelicula/pelicula-listado/pelicula-listado.component';
+import { ActorAltaComponent } from './components/actor/actor-alta/actor-alta.component';
+import { ActorListadoComponent } from './components/actor/actor-listado/actor-listado.component';
+import { TablaPeliculaComponent } from './components/pelicula/tabla-pelicula/tabla-pelicula.component';
 
+const routes: Routes = [
+  { path: '/busqueda', component: BusquedaComponent },
+  { path: '/pelicula/alta', component: PeliculaAltaComponent },
+  { path: '/actor/alta', component: ActorAltaComponent },
+  { path: '/actor/listado', component: ActorListadoComponent },
+  { path: '/pelicula/listado', component: PeliculaListadoComponent },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
